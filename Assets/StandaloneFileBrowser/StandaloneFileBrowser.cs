5f171c3c822d5f708fcd36828921f91aa5f978e1
RicardoEPRodrigues <ricardo.e.p.rodrigues@gmail.com>
2018-09-30 16:33:01 +0100

    Added Linux support (WIP)
    Copied Mac OS File Browser c# and added similar behavior.
    
    Used C and GTK to create a Dialog to select files.
    
    DOES NOT WORK: file saving and Async.

--- Assets/StandaloneFileBrowser/StandaloneFileBrowser.cs
+++ Assets/StandaloneFileBrowser/StandaloneFileBrowser.cs
@@ -19,6 +19,8 @@ namespace SFB {
             _platformWrapper = new StandaloneFileBrowserMac();
 #elif UNITY_STANDALONE_WIN
             _platformWrapper = new StandaloneFileBrowserWindows();
+#elif UNITY_STANDALONE_LINUX
+            _platformWrapper = new StandaloneFileBrowserLinux();
 #elif UNITY_EDITOR
             _platformWrapper = new StandaloneFileBrowserEditor();
 #endif
