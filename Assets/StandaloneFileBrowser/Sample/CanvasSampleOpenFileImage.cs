ae864e858ef056e67892ef95aadbbb868a5ad686
shutosg <shutoreds@gmail.com>
2021-04-03 23:31:18 +0900

    move sample codes to SFB.Samples namespace

--- Assets/StandaloneFileBrowser/Sample/CanvasSampleOpenFileImage.cs
+++ Assets/StandaloneFileBrowser/Sample/CanvasSampleOpenFileImage.cs
@@ -5,11 +5,13 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-using SFB;
 
-[RequireComponent(typeof(Button))]
-public class CanvasSampleOpenFileImage : MonoBehaviour, IPointerDownHandler {
-    public RawImage output;
+namespace SFB.Samples
+{
+    [RequireComponent(typeof(Button))]
+    public class CanvasSampleOpenFileImage : MonoBehaviour, IPointerDownHandler
+    {
+        public RawImage output;
 
 #if UNITY_WEBGL && !UNITY_EDITOR
     //
@@ -27,27 +29,32 @@ public class CanvasSampleOpenFileImage : MonoBehaviour, IPointerDownHandler {
         StartCoroutine(OutputRoutine(url));
     }
 #else
-    //
-    // Standalone platforms & editor
-    //
-    public void OnPointerDown(PointerEventData eventData) { }
+        //
+        // Standalone platforms & editor
+        //
+        public void OnPointerDown(PointerEventData eventData) { }
 
-    void Start() {
-        var button = GetComponent<Button>();
-        button.onClick.AddListener(OnClick);
-    }
-
-    private void OnClick() {
-        var paths = StandaloneFileBrowser.OpenFilePanel("Title", "", ".png", false);
-        if (paths.Length > 0) {
-            StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
+        void Start()
+        {
+            var button = GetComponent<Button>();
+            button.onClick.AddListener(OnClick);
         }
-    }
+
+        private void OnClick()
+        {
+            var paths = StandaloneFileBrowser.OpenFilePanel("Title", "", ".png", false);
+            if (paths.Length > 0)
+            {
+                StartCoroutine(OutputRoutine(new System.Uri(paths[0]).AbsoluteUri));
+            }
+        }
 #endif
 
-    private IEnumerator OutputRoutine(string url) {
-        var loader = new WWW(url);
-        yield return loader;
-        output.texture = loader.texture;
+        private IEnumerator OutputRoutine(string url)
+        {
+            var loader = new WWW(url);
+            yield return loader;
+            output.texture = loader.texture;
+        }
     }
 }
